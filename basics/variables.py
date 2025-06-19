173865d3a3c94efdf2813384534d0aba1c2d918f
Kendrick <kejomarie928@gmail.com>
2025-06-19 14:14:35 -0400

    variables.py addition

---
+++ basics/variables.py
@@ -0,0 +1,16 @@
+# Integers
+age = 21
+
+# Float
+pi = 3.14159
+
+# String
+name = "Kejobyte"
+
+# Boolean
+is_cool = True
+
+print("Name:", name)
+print("Age:", age)
+print("Pi:", pi)
+print("Is cool?", is_cool)
