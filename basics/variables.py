5526075952bf263833a52a823545a7edc4717353
Kendrick <kejomarie928@gmail.com>
2025-10-03 14:34:59 -0400

    Update variables.py

--- basics/variables.py
+++ basics/variables.py
@@ -2,15 +2,12 @@
 age = 21
 
 # Float
-pi = 3.14159
+pi = 3.1459
+temperature = 86.4
 
 # String
-name = "Kejobyte"
+name = "kejobyte"
+city = "Atlanta"
 
 # Boolean
 is_cool = True
-
-print("Name:", name)
-print("Age:", age)
-print("Pi:", pi)
-print("Is cool?", is_cool)
